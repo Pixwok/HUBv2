3876e56fc782e19c102754d7feaff5b774c031c9
Pixwok <40206880+Pixwok@users.noreply.github.com>
2020-10-22 10:52:19 +0200

    Connexion + form ajout et suppression

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+include('php/connexion.php');
+
+//DEFINE VAR
+$error = FALSE;
+//Système de connexion
+if (isset($_POST['login'])) {
+    $login = htmlspecialchars($_POST['pseudo']);
+    $passwd = hash('sha256', $_POST['passwd']);
+    $request = "SELECT * FROM account WHERE pseudo='".$login."' AND passwd='".$passwd."'";
+    $res = $connexion->query($request);
+    $data = $res->fetchALL(PDO::FETCH_ASSOC);
+    if (count($data) === 1) {
+        session_start();
+        $_SESSION['login'] = htmlspecialchars($_POST['pseudo']);
+    } else {
+        $error = TRUE;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -17,8 +37,8 @@
         <aside>
             <nav>
                 <ul>
-                    <li><a href=""><i class="fas fa-plus"></i></a></li>
-                    <li><a href=""><i class="fas fa-trash"></i></a></li>
+                    <li><a href="javascript:void(0)" onclick="document.getElementById('addForm').style.display = 'flex';"><i class="fas fa-plus"></i></a></li>
+                    <li><a href="javascript:void(0)" onclick="document.getElementById('removeForm').style.display = 'flex';"><i class="fas fa-trash"></i></a></li>
                 </ul>
                 <?php if ( session_status() === PHP_SESSION_NONE ): ?>
                     <a href="javascript:void(0)" onclick="document.getElementById('loginForm').style.display = 'flex';"><i class="fas fa-user-alt"></i></a>
@@ -30,16 +50,54 @@
         <section>
             <h1>HUB d'application de Pixwok</h1>
             <div class="container">
-            <div class="popup" id="loginForm">
-                <form action="php/auth.php" class="form-login" method="post">
-                    <a href="javascript:void(0)" onclick="document.getElementById('loginForm').style.display = 'none';"><i class="fas fa-times"></i></a>
-                    <input id="login" type="text" placeholder="pseudo" name="pseudo" required>
-                    <input id="passwd" type="password" placeholder="Password" name="passwd" required>
+                <div class="popup" id="loginForm">
+                    <form action="" class="form-login" method="post">
+                        <a href="javascript:void(0)" onclick="document.getElementById('loginForm').style.display = 'none';"><i class="fas fa-times"></i></a>
+                        <input id="login" type="text" placeholder="pseudo" name="pseudo" required>
+                        <input id="passwd" type="password" placeholder="Password" name="passwd" required>
 
-                    <input type="submit" name="login" class="btn-apply" value="Login">
-                </form>
+                        <input type="submit" name="login" class="btn-apply" value="Login">
+                    </form>
+                </div>
             </div>
+            <div class="container">
+                <div class="popup" id=addForm>
+                    <form action="" method="post">
+                        <a href="javascript:void(0)" onclick="document.getElementById('addForm').style.display = 'none';"><i class="fas fa-times"></i></a>
+                        <input type="text" name="title" id="title" required>
+                        <input type="text" name="link" id="link" required>
+                        <select name="tag" id="tag" required>
+                            <option value="">Choix du tag</option>
+                            <?php foreach ($data as $value): ?>
+                                <option value="<?=$value['id']?>"><?=$value['nom']?></option>
+                            <?php endforeach; ?>
+                        </select>
+                        <input type="submit" name="addButton" value="Ajouter">
+                    </form>
+                </div>
             </div>
+            <div class="container">
+                <div class="popup" id=removeForm>
+                    <form action="" method="post">
+                        <a href="javascript:void(0)" onclick="document.getElementById('removeForm').style.display = 'none';"><i class="fas fa-times"></i></a>
+                        <select name="tag" id="tag" required>
+                            <option value="">Sélectionner un bouton</option>
+                            <?php foreach ($data as $value): ?>
+                                <option value="<?=$value['id']?>"><?=$value['title']?></option>
+                            <?php endforeach; ?>
+                            <input type="submit" name="removeButton" value="Supprimer">
+                        </select>
+                    </form>
+                </div>
+            </div>
+
+            <?php if ($error === TRUE): ?>
+            <div class="container-error">
+                <div class="error-box">
+                    <span>Erreur de connexion</span>
+                </div>
+            </div>
+            <?php endif;?>
         </section>
     </main>
 </body>
